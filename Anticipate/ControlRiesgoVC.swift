79447121aefbd4f6b0ff3e2eee1d4c18756487ea
miwesfx <miwesfx@macceimar01.uca.es>
2016-04-18 15:24:58 +0200

    tableviewcontroller en tabla de vídeos

---
+++ Anticipate/ControlRiesgoVC.swift
@@ -0,0 +1,24 @@
+//
+//  ControlRiesgoVC.swift
+//  Anticipate
+//
+//  Created by Miwe sfx on 18/4/16.
+//  Copyright © 2016 Miguel Pérez Prado. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class ControlRiesgoVC: UITableViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
